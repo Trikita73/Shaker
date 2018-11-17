508142a8d28366f8fb02264512dfe2eebfa940e6
Trikita73 <diachenkoandrii7@gmail.com>
2018-11-17 16:50:09 +0200

    shaker

---
+++ js/common.js
@@ -0,0 +1,34 @@
+$(function() {
+
+
+	$('.owl-carousel').owlCarousel({
+    rtl:true,
+		loop:true,
+		//margin:10,
+		navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
+		nav:true,
+		dots: false,
+    responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 2
+			},
+			992: {
+				items: 3
+			},
+			1200: {
+				items: 4
+			}
+		}
+	});
+
+	$(document).ready(function() {
+    $("a.gallery").fancybox();
+	});
+	
+
+});
+
+
